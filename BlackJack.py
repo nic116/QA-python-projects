24a6863fc7f5513334b2b9657297c144a71b1702
nic116 <nicolas_abadi@hotmail.com>
2020-02-26 15:41:41 +0000

    Still need to finish this

---
+++ BlackJack.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3.8
+
+import random
+
+# define A,J,Q,K
+#shuffle 2 decks
+class deck:
+    cards:["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+
+    def shuffle(self,cards):
+        readydeck=[random.shuffle(cards)]
+        return readyreck
+
+    def deal(self, readydeck) :
+        self.dealerhand = [self.readydeck(1), self.readydeck(3)]
+        self.playerhand = [self.readydeck(0), self.readydeck(2)]
+        return self.dealerhand
+        return self.dealerhand
+
+class Player
+    def decision(self, choices, answer):
+        self.choices = ["h","s"]
+        self.answer = input(self.choices)
+
+
+
+class Dealer
+    dealerhand=[readydeck(1),readydeck(3)]  #dealer gets 2 cards (shows only one)
+    pass
+
+
+
+
+
+#Player turn: stick or hit?
+
+# dealer's turn: hit or stick?
+
